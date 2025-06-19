25341ad1a7fdd5c7d1580b8b1598d5de959e58a3
Terry294-byte <shiroterry168@gmail.com>
2025-06-19 14:13:34 +0300

    added the alumni folder with create and index pages

---
+++ resources/views/alumni/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>Add New Alumni</h2>
+
+@if ($errors->any())
+    <div style="color: red;">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('alumni.store') }}" method="POST">
+    @csrf
+    <label>Name:</label><br>
+    <input type="text" name="name" value="{{ old('name') }}"><br><br>
+
+    <label>Email:</label><br>
+    <input type="email" name="email" value="{{ old('email') }}"><br><br>
+
+    <label>Phone:</label><br>
+    <input type="text" name="phone" value="{{ old('phone') }}"><br><br>
+
+    <label>Course:</label><br>
+    <input type="text" name="course" value="{{ old('course') }}"><br><br>
+
+    <label>Graduation Year:</label><br>
+    <input type="number" name="graduation_year" value="{{ old('graduation_year') }}"><br><br>
+
+    <button type="submit">Save</button>
+</form>
+@endsection
---
+++ resources/views/alumni/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>All Alumni</h2>
+
+@if(session('success'))
+    <p style="color: green">{{ session('success') }}</p>
+@endif
+
+<a href="{{ route('alumni.create') }}">+ Add New Alumni</a>
+
+<table border="1" cellpadding="10" cellspacing="0" style="margin-top: 20px; width: 100%;">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Course</th>
+            <th>Graduation Year</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($alumni as $alum)
+            <tr>
+                <td>{{ $alum->name }}</td>
+                <td>{{ $alum->email }}</td>
+                <td>{{ $alum->phone }}</td>
+                <td>{{ $alum->course }}</td>
+                <td>{{ $alum->graduation_year }}</td>
+                <td>
+                    <form action="{{ route('alumni.destroy', $alum->id) }}" method="POST" onsubmit="return confirm('Delete this alumni?');">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" style="color: red;">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
